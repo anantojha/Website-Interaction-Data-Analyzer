11e91060090503b7a7cd1c4fb3d4ba2b65ba42dc
anantojha <anant.ojha@outlook.com>
2020-03-16 20:16:26 -0400

    text21 analysis done!

--- csv_reader.py
+++ csv_reader.py
@@ -1,6 +1,5 @@
 import csv
 import datetime
-
 ##### Interaction CSV Data Reader #####
 
 #INSTRUCTIONS:
@@ -22,7 +21,11 @@ import datetime
 #explanation and pseudocode for your approach, and the documented source code. Also
 #provide the resulting data in CSV format
 
-
+def roundSeconds(delta):
+	if(datetime.timedelta(microseconds=delta.microseconds) >= datetime.timedelta(microseconds=500000)):
+		return delta - datetime.timedelta(microseconds=delta.microseconds) + datetime.timedelta(seconds=1)
+	else:
+		return delta - datetime.timedelta(microseconds=delta.microseconds)
 
 
 #DOCUMENTS to read and write
@@ -70,7 +73,7 @@ for row in myFile:
 		date_time_obj = datetime.datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S')
 		data = [date_time_obj,row[1],row[3],row[4],row[5],row[6]]
 		list.get(row[1]).append(data)
-		
+
 
 
 
@@ -79,24 +82,51 @@ for x in list:
 	outRow = []
 	number_of_login_success = 0
 	number_of_login_failure = 0
+	Avg_login_time = datetime.timedelta()
+	Avg_login_success = datetime.timedelta()
+	Avg_login_failure = datetime.timedelta()
+	divisors = 0
 	outRow.append(list.get(x)[1][1])
 	outRow.append(list.get(x)[1][2])
-	for k in list.get(x):
-		if(k[4] == 'login'):
-			if(k[5] == 'success'):
+	for k in range(0,len(list.get(x))-1):
+		if(list.get(x)[k][4] == 'login'):
+			if(list.get(x)[k][5] == 'success'):
 				number_of_login_success = number_of_login_success + 1
-				#while():
-
+				currIndex = k - 1
+				while(list.get(x)[currIndex][5] != 'start'):
+					currIndex = currIndex - 1
+				#print(list.get(x)[currIndex])
+				#print(list.get(x)[k])
+				#print(list.get(x)[k][0] - list.get(x)[currIndex][0])
+				Avg_login_success = Avg_login_success + (list.get(x)[k][0] - list.get(x)[currIndex][0])
+				#print(Avg_login_success)
 			else:
 				number_of_login_failure = number_of_login_failure + 1
-				#while():
+				currIndex = k - 1
+				while(list.get(x)[currIndex][5] != 'start'):
+					currIndex = currIndex - 1
+				#print(list.get(x)[currIndex])
+				#print(list.get(x)[k])
+				#print(list.get(x)[k][0] - list.get(x)[currIndex][0])
+				Avg_login_failure = Avg_login_failure + (list.get(x)[k][0] - list.get(x)[currIndex][0])
+				#print(Avg_login_failure)
+	print(list.get(x)[1][1])
+	#if(number_of_login_success > 0):
+		#print("Average Success Time: " + str(Avg_login_success/number_of_login_success))
+	#if(number_of_login_failure > 0):
+		#print("Average Fail Time: " + str(Avg_login_failure/number_of_login_failure))
+	Avg_login_time = (Avg_login_success + Avg_login_failure)/(number_of_login_success + number_of_login_failure)
+	print(Avg_login_time)
 
 	outRow.append(number_of_login_success + number_of_login_failure)
 	outRow.append(number_of_login_success)
 	outRow.append(number_of_login_failure)
-	outRow.append(0)
-	outRow.append(0)
-	outRow.append(0)
+	outRow.append(roundSeconds(Avg_login_time))
+	outRow.append(roundSeconds(Avg_login_success/number_of_login_success))
+	if(number_of_login_failure > 0):
+		outRow.append(roundSeconds(Avg_login_failure/number_of_login_failure))
+	else:
+		outRow.append(0)
 	outFile.writerow(outRow)
 
 
