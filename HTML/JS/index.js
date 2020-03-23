04a6874150afcbbede1161a5f5ecbaac9eaf475d
etoy22 <ethanleider@gmail.com>
2020-03-23 18:57:03 -0400

    Update index.js

--- HTML/JS/index.js
+++ HTML/JS/index.js
@@ -1,40 +1,75 @@
 var mystring ="";
 var symbols ="";
-for(let i = 0;i<7;i++){
-    var temp = Math.floor(Math.random() * 8) + 1;
-    if (temp == 1){
-        mystring += "w";
-        symbols += "↑";
-    }
+var trackstr = ["","",""];
+var tracksym = ["","",""];
+var count = 0;
 
-    else if (temp == 2){
-        mystring += "q";
-        symbols += "↖";;
+if(localStorage.getItem("count")==null){
+    localStorage.setItem("count", 0);
+}
+
+if(localStorage.getItem("str")==null){
+    localStorage.setItem("str", JSON.stringify(["","",""]));
+}
+
+if(localStorage.getItem("sym")==null){
+    localStorage.setItem("sym", JSON.stringify(["","",""]));
+}
+
+function random(){
+    count = localStorage.getItem("counting");
+    for(let i = 0;i<7;i++){
+        var temp = Math.floor(Math.random() * 8) + 1;
+        if (temp == 1){
+            mystring += "w";
+            symbols += "↑";
+        }
+
+        else if (temp == 2){
+            mystring += "q";
+            symbols += "↖";;
+        }
+        else if (temp == 3){
+            mystring += "e";
+            symbols += "↗"
+        }
+        else if (temp == 4){
+            mystring += "a";
+            symbols += "←";
+        }
+        else if (temp == 5){
+            mystring += "d";
+            symbols += "→";
+        }
+        else if (temp == 6){
+            mystring += "z";
+            symbols += "↙";
+        }
+        else if (temp == 7){
+            mystring += "x";
+            symbols += "↓";
+        }
+        else if (temp == 8){
+            mystring += "c";
+            symbols += "↘";
+        }
+        
     }
-    else if (temp == 3){
-        mystring += "e";
-        symbols += "↗"
+    var temp = JSON.parse(localStorage.getItem("str"));
+    var temp2 = JSON.parse(localStorage.getItem("sym"));
+    tracksym [localStorage.getItem("count")] = symbols;
+    trackstr[localStorage.getItem("count")] = mystring;
+    for(let i = 0; i < 3; i++){
+        if(trackstr[i] == ""){
+            trackstr[i] = temp[i];
+        }
+        if(tracksym[i] == ""){
+            tracksym[i] = temp2[i];
+        }
     }
-    else if (temp == 4){
-        mystring += "a";
-        symbols += "←";
-    }
-    else if (temp == 5){
-        mystring += "d";
-        symbols += "→";
-    }
-    else if (temp == 6){
-        mystring += "z";
-        symbols += "↙";
-    }
-    else if (temp == 7){
-        mystring += "x";
-        symbols += "↓";
-    }
-    else if (temp == 8){
-        mystring += "c";
-        symbols += "↘";
-    }
+    localStorage.setItem("str", JSON.stringify(trackstr));
+    localStorage.setItem("sym", JSON.stringify(tracksym));
+
 }
 
 function entered(str){
@@ -43,6 +78,25 @@ function entered(str){
     if(str == mystring)
         alert("You got it right");
     else
-        alert("You got it wrong it");
-    
+        alert("You got it wrong it");    
+}
+
+function done(){
+    location.replace("Index.html");
+}
+
+function increase(){
+    if(parseInt(localStorage.getItem("count")) < 2){
+        localStorage.setItem("count", parseInt(localStorage.getItem("count"))+1);
+    }
+}
+function opened(){
+    console.log("test");
+    location.replace("Password.html");
+    console.log("test");
+}
+
+
+if (performance.navigation.type == 1) {
+    localStorage.clear();
 }
