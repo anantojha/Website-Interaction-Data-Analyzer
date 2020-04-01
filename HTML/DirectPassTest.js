8390d14c3da7b72155351b217bd6f8b93807d88b
etoy22 <ethanleider@gmail.com>
2020-04-01 08:20:37 -0400

    Update DirectPassTest.js
    Added comments

--- HTML/DirectPassTest.js
+++ HTML/DirectPassTest.js
@@ -1,4 +1,11 @@
+/*
+    app.post('/data') - sends info to the csv from the html
+    app.get('/') - takes you to the home page/ DirectPass.ejs
+    app.get('/user') - sends the info to the form of what user they are
+    app.get('/Password.html') - sends you to the Password.ejs
+    app.get('/Testing.html') - sends you to the Testing.ejs
 
+*/
 var express = require('express');
 var fs = require('fs');
 var id = 100;
